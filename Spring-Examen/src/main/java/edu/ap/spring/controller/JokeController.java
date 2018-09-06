176b72543f41cc93d4a3c1712ac643c43848faeb
ezravm <33087661+ezravm@users.noreply.github.com>
2018-09-06 15:01:12 +0200

    joke.jsp & logic
    added the joke.jsp and the logic needed to get a random joke with the requested parameters from the remote api.

--- Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
+++ Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
@@ -4,17 +4,70 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 @Controller
 @Scope("session")
 public class JokeController {
-   
-   public JokeController() {
+
+    private HttpURLConnection con = null;
+    public static final String REQUEST_METHOD = "POST";
+
+
+    public JokeController() {
    }
        
    @RequestMapping("/joke")
-   public String joke() {
-	   return "";
-   }
+   public String joke(Map<String, Object > model, @RequestParam("firstName") String firstname,
+                      @RequestParam("lastName") String lastname) {
+        String responseString = "";
+        try {
+            URL conUrl = new URL("http://api.icndb.com/jokes/random?firstName=" + firstname + "&lastName=" + lastname );
+            System.out.println(conUrl);
+            con = (HttpURLConnection) conUrl.openConnection();
+            con.setRequestMethod("GET");
+            con.setDoOutput(true);
+            int responsecode = con.getResponseCode();
+            System.out.println(responsecode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            System.out.println(response.toString());
+
+            JSONObject obj = new JSONObject(response.toString());
+            JSONObject obj2 = (JSONObject) obj.get("value");
+            System.out.println(obj2);
+            responseString = obj2.get("joke").toString();
+
+        }
+        catch(IOException ex){
+            System.out.println("http error");
+       }
+
+
+       model.put("response",responseString);
+
+
+       return "joke" ;
+    }
+
 		   
    @RequestMapping("/joke_post")
    public String joke_post() {
---
+++ Spring-Examen/src/main/webapp/views/joke.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Work
+  Date: 6/09/2018
+  Time: 14:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Graptje</title>
+</head>
+<body>
+<h1>Welkom</h1>
+<br>
+<h2>het graptje: </h2>
+<h3>
+${response}
+</h3>
+</body>
+</html>
